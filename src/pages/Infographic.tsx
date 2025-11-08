2d645649e48f8fae3546e03ef808f8aca972775c
Taleeb Ahmad <taleeb065@gmail.com>
2025-11-08 19:53:56 +0500

    Update Infographic.tsx

--- src/pages/Infographic.tsx
+++ src/pages/Infographic.tsx
@@ -64,7 +64,7 @@ const Infographic = () => {
         </div>
       </section>
 
-      <main className="py-12 px-6 md:px-12 lg:px-24">
+      <main className="py-12 px-6 md:px-12 lg:px-24 info_div_g">
         <div className="max-w-7xl mx-auto">
 
           {loading ? (
