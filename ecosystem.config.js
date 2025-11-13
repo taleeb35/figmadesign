67496625572073f5e119655ed89b9787d45df433
Moaz Ateeq <moazateeq@gmail.com>
2025-11-13 12:02:44 +0500

    update ecosystem 1202

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,35 @@ module.exports = {
     {
       name: "theannualreports",
       cwd: "/home/theannualreports/htdocs/theannualreports.com",
-      script: "./pm2-start.sh",
+      script: "npm",
+      args: "run preview",
       env: { NODE_ENV: "production", PORT: "3000" }
     }
   ]
 };
+
+// module.exports = {
+//   apps: [
+//     {
+//         name: "theannualreports",
+//         cwd: "/home/theannualreports/htdocs/theannualreports.com",
+//         //script: "./pm2-start.sh",
+//         script: "npm",
+//         args: "run preview",
+//         env: { NODE_ENV: "production", PORT: "3000" }
+//     }
+//   ]
+// };
+
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: "theannualreports",
+//       cwd: "/home/theannualreports/htdocs/theannualreports",
+//       script: "npm",
+//       args: "run preview",
+//       env: { NODE_ENV: "production", PORT: "3051" }
+//     }
+//   ]
+// };
