9029954429ab7f32c05fcb42a38e7ee4ad72de2d
Moaz Ateeq <moazateeq@gmail.com>
2025-11-13 11:34:18 +0500

    update deployment 1134

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: "theannualreports",
-      cwd: "/home/theannualreports/htdocs/theannualreports",
-      script: "npm",
-      args: "run preview",
+      cwd: "/home/theannualreports/htdocs/theannualreports.com",
+      script: "./pm2-start.sh",
       env: { NODE_ENV: "production", PORT: "3000" }
     }
   ]
