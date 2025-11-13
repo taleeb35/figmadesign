1fd82a3149bb1ed103946aff944ca51f02de23f4
Moaz Ateeq <moazateeq@gmail.com>
2025-11-13 13:23:43 +0500

    update deployment 1323

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,3 +9,16 @@ module.exports = {
     }
   ]
 };
+
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: "theannualreports",
+//       cwd: "/home/theannualreports/htdocs/theannualreports.com",
+//       script: "npm",
+//       args: "run preview",
+//       env: { NODE_ENV: "production", PORT: "3000" }
+//     }
+//   ]
+// };
