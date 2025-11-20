e567d671282410f5b2e824121558ff5419c61eca
Taleeb Ahmad <taleeb065@gmail.com>
2025-11-20 08:25:56 +0500

    Update ClientLogosManager.tsx

--- src/components/admin/ClientLogosManager.tsx
+++ src/components/admin/ClientLogosManager.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "../../admin.css";
+import "../../../admin.css";
 import { supabase } from "@/integrations/supabase/client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
